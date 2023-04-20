f3cc558c8c9cfec27ac3a14385cc1a511dd9b2ed
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-04-20 12:22:54 -0700

    small updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import "./app.css"
+  import "./app.css";
 </script>
 
-<div class="flex flex-col justify-center items-center gap-4 w-full h-full p-4">
+<div class="flex h-full w-full flex-col items-center justify-center gap-4 p-4">
   <h1 class="text-xl">Hello World</h1>
 </div>
