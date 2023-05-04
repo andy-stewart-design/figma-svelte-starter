4d2fdc59bb65cdda75a1644a25f943aa08ae81e3
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2023-05-04 06:31:42 -0700

    Fixing Tailwind prettier integration

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import "./app.css";
+	import './app.css';
 </script>
 
 <div class="flex h-full w-full flex-col items-center justify-center gap-4 p-4">
-  <h1 class="text-xl">Hello World</h1>
+	<h1 class="text-xl">Hello World</h1>
 </div>
